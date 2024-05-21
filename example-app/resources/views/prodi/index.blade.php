dd1d089e00d23b170fe7a5e69d249f2e9b4f35bb
PiolaV <160827591+PiolaV@users.noreply.github.com>
2024-05-21 17:01:59 +0700

    spica21

--- example-app/resources/views/prodi/index.blade.php
+++ example-app/resources/views/prodi/index.blade.php
@@ -1,20 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Program Studi</title>
-</head>
-<body>
-    <h1>UMDP</h1>
-    <h2>Program Studi</h2>
-    <ul>
-    @foreach ($prodi as $item)
-        <li>{{ $item ["nama"] }}  {{$item ["singkatan" ]}}
-        {{ $item["Fakultas"]["nama"] }}</li>
-    @endforeach
-    </ul>
+@extends('layout.main')
+
+@section('title','Prodi')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 grid-margin stretch-card">
+    <div class="card">
+        <div class="card-body">
+        <h4 class="card-title">Prodi</h4>
+        <p class="card-description">
+          Add class <code>.table</code>
+          list data Prodi
+        </p>
+        <div class="table-responsive">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Nama Prodi</th>
+                <th>Singkatan</th>
+                <th>Nama Fakultas</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($prodi as $item)
+                <tr>
+                    <td>{{ $item ["nama"] }} </td>
+                    <td>{{$item ["singkatan" ]}}</td>
+                    <td>{{$item ["fakultas"] ["nama"]}}</td>
+                 @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
     
-</body>
-</html>
